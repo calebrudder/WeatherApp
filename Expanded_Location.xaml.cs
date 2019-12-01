0879c522abaf6083ce51f6cee440fdba833872b5
JonathanSCasper <54650269+JonathanSCasper@users.noreply.github.com>
2019-12-01 16:32:14 -0600

    Got Expanded Page started

--- Expanded_Location.xaml.cs
+++ Expanded_Location.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Weather.DataAccess;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -12,6 +13,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Media.Imaging;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -22,9 +24,24 @@ namespace Weather
     /// </summary>
     public sealed partial class Expanded_Location : Page
     {
+
         public Expanded_Location()
         {
             this.InitializeComponent();
+            weather();
+        }
+        public async void weather()
+        {
+            int zip = 72149;
+            RootObject myWeather = await WeatherMap.GetWeather(zip, "imperial");
+            string cityName = myWeather.name.ToString();
+            City_Name.Text = cityName;
+            string temp = myWeather.main.temp.ToString();
+            Current_Temp.Text = temp;
+            string Temp_low = myWeather.main.temp_min.ToString();
+            Temp_Low.Text = Temp_low;
+            string Temp_high = myWeather.main.temp_max.ToString();
+            Temp_High.Text = Temp_high;
         }
     }
 }
