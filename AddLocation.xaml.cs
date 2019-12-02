b3128cd7f6138f55842a46de83851b14d8b45e5f
JonathanSCasper <54650269+JonathanSCasper@users.noreply.github.com>
2019-12-01 22:13:51 -0600

    Formatted Add Location

--- AddLocation.xaml.cs
+++ AddLocation.xaml.cs
@@ -26,5 +26,10 @@ namespace Weather
         {
             this.InitializeComponent();
         }
+
+        private void AddCity_Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
