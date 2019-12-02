19688892ccf1c830c1eb6a618921ea04ae9451e0
JonathanSCasper <54650269+JonathanSCasper@users.noreply.github.com>
2019-12-02 00:20:33 -0600

    Started json

--- Home.xaml.cs
+++ Home.xaml.cs
@@ -1,19 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using Weather.DataAccess;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Imaging;
-using Windows.UI.Xaml.Navigation;
+
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -32,14 +23,28 @@ namespace Weather
         }
         public async void weather()
         {
-            //if() //if user exists, set to specifications, else do the following
-            int zip = 72149;
+            int zip;
+
+            if(ApplicationData.Current.LocalSettings.Values.ContainsKey("json"))
+            {
+                string json = ApplicationData.Current.LocalSettings.Values["json"] as string;
+                // UserObject = JsonConvert.DeserializeObject<>(json);
+                zip = 1;//UserObject.Zip;
+                //message.Text = "Welcome, " + UserObject.Name + ", the weather looks "
+                //temperature.Text = "It is currently " + UserObject.Temp + " degrees in " + UserObject.
+            }
+            else
+            {
+                zip = 72149;
+            }
             RootObject myWeather = await WeatherMap.GetWeather(zip, "imperial");
             string icon = String.Format("http://openweathermap.org/img/wn/{0}.png", myWeather.weather[0].icon);
             result.Source = new BitmapImage(new Uri(icon, UriKind.Absolute));
 
             string temp = myWeather.main.temp.ToString();
             temperature.Text = temp;
+
+            temperature.Text = "It is currently " + myWeather.main.temp.ToString() + " degrees in " + myWeather.name.ToString();
 
             string Temp_low = myWeather.main.temp_min.ToString();
             low.Text = Temp_low;
