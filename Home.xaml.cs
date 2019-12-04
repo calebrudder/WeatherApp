c7282148cd0cbca83942ed4fce0d4dac6ed47fdd
JonathanSCasper <jcasper@harding.edui>
2019-12-04 14:08:24 -0600

    Fixed some issues

--- Home.xaml.cs
+++ Home.xaml.cs
@@ -4,6 +4,7 @@ using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
 
 namespace Weather
 {
@@ -15,30 +16,100 @@ namespace Weather
             this.InitializeComponent();
             weather();
         }
+        protected override void OnNavigatedTo(NavigationEventArgs args)
+        {
+            weather();
+        }
         public async void weather()
         {
-            string zip;
+            string zip, system, userName;
+            string Greeting = "";
 
-            if(ApplicationData.Current.LocalSettings.Values.ContainsKey("settings"))
+            if (ApplicationData.Current.LocalSettings.Values.ContainsKey("settings"))
             {
                 var settings = ApplicationData.Current.LocalSettings.Values["settings"] as ApplicationDataCompositeValue;
 
+                userName = settings["Name"].ToString();
                 zip = settings["DefaultZip"].ToString();
-                // UserObject = JsonConvert.DeserializeObject<>(json);
-                //UserObject.Zip;
-                //message.Text = "Welcome, " + UserObject.Name + ", the weather looks "
-                //temperature.Text = "It is currently " + UserObject.Temp + " degrees in " + UserObject.
+
+                if ((bool)settings["Metric"] == true)
+                {
+                    system = "metric";
+                }
+                else
+                {
+                    system = "imperial";
+                }
+
             }
             else
             {
                 zip = "72149";
+                userName = "";
+                system = "imperial";
             }
-            RootObject myWeather = await WeatherMap.GetWeather(zip, "imperial");
+            RootObject myWeather = await WeatherMap.GetWeather(zip, system);
             string icon = String.Format("http://openweathermap.org/img/wn/{0}.png", myWeather.weather[0].icon);
             result.Source = new BitmapImage(new Uri(icon, UriKind.Absolute));
 
+
+            // Get local time
+            string time = DateTime.Now.ToString("h:mm tt");
+            string hour = time[0].ToString();
+            if (time[1] != ':')
+            {
+                hour += time[1].ToString();
+            }
+            string AmPm = time[5].ToString();
+            AmPm += time[6].ToString();
+            AmPm.Trim();
+
+            int Hour = Int32.Parse(hour);
+
+            // Determine if it is evening, afternoon, or morning
+            if (Hour == 12 || Hour >= 1 && Hour <= 7 && AmPm == "PM")
+            {
+                Greeting = "Good afternoon " + userName;// + ", the weather is " + myWeather;
+                greeting.Text = Greeting;
+            }
+            else if (Hour >= 8 && AmPm == "PM")
+            {
+                Greeting = "Good Evening " + userName;// + ", the weather is " + myWeather.weather;
+                greeting.Text = Greeting;
+            }
+            else if (Hour >= 1 && Hour <= 11 && AmPm == "PM")
+            {
+                Greeting = "Good Morning " + userName;// + ", the weather is " + myWeather.weather;
+                greeting.Text = Greeting;
+            }
+
             string temp = myWeather.main.temp.ToString();
             temperature.Text = temp;
+
+            // Determine the temperature
+            string Temp = temp.Remove(2);
+            int Temperature = Int32.Parse(Temp);
+
+            if (Temperature <= 60 && Temperature >= 40 && system == "imperial")
+            {
+                Greeting += ", the temperature is cool";
+                greeting.Text = Greeting;
+            }
+            else if (Temperature >= 61 && Temperature <= 80 && system == "imperial")
+            {
+                Greeting += ", the temperature is nice";
+                greeting.Text = Greeting;
+            }
+            else if (Temperature <= 59 && Temperature >= 33 && system == "imperial")
+            {
+                Greeting += ", the temperature is cold";
+                greeting.Text = Greeting;
+            }
+            else if (Temperature <= 32 && system == "imperial")
+            {
+                Greeting += ", the temperature is freezing";
+                greeting.Text = Greeting;
+            }
 
             temperature.Text = "It is currently " + myWeather.main.temp.ToString() + " degrees in " + myWeather.name.ToString();
 
@@ -49,8 +120,6 @@ namespace Weather
             string Temp_wind = myWeather.wind.speed.ToString();
             wind.Text = Temp_wind;
 
-            string cityName = myWeather.name;
-            greeting.Text = cityName;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
